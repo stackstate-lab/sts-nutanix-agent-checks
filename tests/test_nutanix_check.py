91a33d73a3eec967d4cbb9ade11fb37d8e160eed
Ravan Naidoo <rnaidoo@stackstate.com>
2022-06-24 16:11:17 +0200

    refactored vm templates to use v3 api

--- tests/test_nutanix_check.py
+++ tests/test_nutanix_check.py
@@ -25,40 +25,7 @@ def test_check(m: requests_mock.Mocker = None):
     check = NutanixCheck("nutanix", {}, {}, instances=[instance_dict])
     check._init_health_api()
 
-    nutanix = NutanixClient(instance.nutanix, logger)
-    print(nutanix.get_url(nutanix.V2, "clusters"))
-    m.register_uri("GET", nutanix.get_url(nutanix.V2, "clusters"), json=response("get_clusters_v2"))
-    print(nutanix.get_url(nutanix.V1_BETA_KARBON, "k8s/clusters"))
-    m.register_uri(
-        "GET", nutanix.get_url(nutanix.V1_BETA_KARBON, "k8s/clusters"), json=response("karbon_list_k8s_clusters")
-    )
-    print(nutanix.get_url(nutanix.V1_ALPHA_KARBON, "k8s/clusters/stackstate/node-pools"))
-    m.register_uri(
-        "GET",
-        nutanix.get_url(nutanix.V1_ALPHA_KARBON, "k8s/clusters/stackstate/node-pools"),
-        json=response("karbon_list_cluster_node_pools"),
-    )
-
-    print(nutanix.get_url(nutanix.V3, "hosts/list"))
-    m.register_uri(
-        "POST",
-        nutanix.get_url(nutanix.V3, "hosts/list"),
-        json=response("list_hosts"),
-    )
-
-    print(nutanix.get_url(nutanix.V2, "disks"))
-    m.register_uri(
-        "GET",
-        nutanix.get_url(nutanix.V2, "disks"),
-        json=response("get_disks_v2"),
-    )
-
-    print(nutanix.get_url(nutanix.V2, "vms?include_vm_disk_config=true"))
-    m.register_uri(
-        "GET",
-        nutanix.get_url(nutanix.V2, "vms?include_vm_disk_config=true"),
-        json=response("get_vms_include_disk_v2"),
-    )
+    _setup_request_mocks(instance, m)
 
     check.check(instance)
     stream = {"urn": "urn:health:nutanix:nutanix_health", "sub_stream": ""}
@@ -68,8 +35,8 @@ def test_check(m: requests_mock.Mocker = None):
     snapshot = topology.get_snapshot("")
     components = snapshot["components"]
     relations = snapshot["relations"]
-    assert len(components) == 99, "Number of Components does not match"
-    assert len(relations) == 108, "Number of Relations does not match"
+    assert len(components) == 102, "Number of Components does not match"
+    assert len(relations) == 110, "Number of Relations does not match"
     assert len(health_check_states) == 8, "Number of Health does not match"
     assert len(metric_names) == 4, "Number of Metrics does not match"
 
@@ -82,10 +49,29 @@ def test_check(m: requests_mock.Mocker = None):
     assert host_component["data"]["custom_properties"]["ipv4_address"] == "10.55.90.119"
 
 
+def _setup_request_mocks(instance, m):
+    nutanix = NutanixClient(instance.nutanix, logger)
 
-def response(file_name):
-    with open(f"tests/resources/responses/{file_name}.json") as f:
-        return json.load(f)
+    def response(file_name):
+        with open(f"tests/resources/responses/{file_name}.json") as f:
+            return json.load(f)
+
+    def register(method, api_version, uri, json_file):
+        url = nutanix.get_url(api_version, uri)
+        print(url)
+        m.register_uri(method, url, json=response(json_file))
+
+    endpoints = [("GET", nutanix.V2, "clusters", "get_clusters_v2"),
+                 ("GET", nutanix.V1_BETA_KARBON, "k8s/clusters", "karbon_list_k8s_clusters"),
+                 ("GET", nutanix.V1_ALPHA_KARBON, "k8s/clusters/stackstate/node-pools", "karbon_list_cluster_node_pools"),
+                 ("POST", nutanix.V3, "hosts/list", "list_hosts"),
+                 ("POST", nutanix.V3, "vms/list", "listvms"),
+                 ("GET", nutanix.V2, "disks", "get_disks_v2"),
+                 ("GET", nutanix.V2, "networks", "get_networks_v2"),
+                 ]
+
+    for endpoint in endpoints:
+        register(*endpoint)
 
 
 def setup_test_instance() -> Dict[str, Any]:
